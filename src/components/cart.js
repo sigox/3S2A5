459f9dd7f245052d452f618fc50a67a42512bdde
sigox <131880018+sigox@users.noreply.github.com>
2023-09-13 20:30:19 +0800

    add props

--- src/components/cart.js
+++ src/components/cart.js
@@ -17,36 +17,45 @@ const CartData =[
     },
 ];
 
-export default function cart() {
-    const CartInner = CartData.map ( Data =>
-        <div class="product-container col col-12" data-count="0" data-price={Data.price}>
-            <img class="img-container"src={Data.img} alt="" />
-            <div class="product-info">
-                <div class="product-name">{Data.name}</div>
-                <div class="product-control-container">
-                    <div class="product-control">
-                        <img src={minus} class="product-action minus" alt="" />
-                        <span class="product-count">0</span>
-                        <img src={plus} class="product-action plus" alt="" />
+function CartItem ({item}) {
+    return (
+        <div className="product-container col col-12" data-count={item.quantity} data-price={item.price}>
+            <img className="img-container"src={item.img} alt="" />
+            <div className="product-info">
+                <div className="product-name">{item.name}</div>
+                <div className="product-control-container">
+                    <div className="product-control">
+                        <img src={minus} className="product-action minus" alt="" />
+                        <span className="product-count">{item.quantity}</span>
+                        <img src={plus} className="product-action plus" alt="" />
                     </div>
                 </div>
-                <div class="price">$0</div>
+                <div className="price">${item.quantity*item.price}</div>
             </div>
         </div>
     )
+}
+
+export default function cart() {
+    const CartInner = CartData.map ( Data => {
+        return(
+            <CartItem item={Data} />
+        );
+    })
+    console.log(CartData)
     return(
-        <section class="cart-container col col-lg-5 col-sm-12">
-            <h3 class="cart-title">購物籃</h3>
-            <section class="product-list col col-12" data-total-price="0">
+        <section className="cart-container col col-lg-5 col-sm-12">
+            <h3 className="cart-title">購物籃</h3>
+            <section className="product-list col col-12" data-total-price="0">
                 {CartInner}
             </section>
-            <section class="cart-info shipping col col-12">
-                <div class="text">運費</div>
-                <div class="price">免費</div>
+            <section className="cart-info shipping col col-12">
+                <div className="text">運費</div>
+                <div className="price">免費</div>
             </section>
-            <section class="cart-info total col col-12">
-                <div class="text">小計</div>
-                <div class="price">$0</div>
+            <section className="cart-info total col col-12">
+                <div className="text">小計</div>
+                <div className="price">$0</div>
             </section>
         </section>
     )
